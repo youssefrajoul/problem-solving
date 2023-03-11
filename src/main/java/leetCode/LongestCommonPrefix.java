185226a24f15e17f8e6b08e8d34bc83c434fbf3d
59939 <59939@etu.he2b.be>
2023-03-11 16:48:48 +0100

    longest common prefix solved

--- src/main/java/leetCode/LongestCommonPrefix.java
+++ src/main/java/leetCode/LongestCommonPrefix.java
@@ -1,11 +1,29 @@
-/*
 package leetCode;
 
 public class LongestCommonPrefix {
-    public String longestCommonPrefix(String[] strs) {
-        for (int i = 0; i < strs.length; i++) {
-            strs[i]
+    public static String longestCommonPrefixMehod(String[] strs) {
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            String tempPrefix = "";
+            for (int j = 0; j < strs[i].length(); j++) {
+                if (strs[i].length() == 0) return "";
+                if (prefix.charAt(0) != strs[i].charAt(0)) {
+                    return "";
+                }
+                if (j < prefix.length() && j < strs[i].length() && prefix.charAt(j) == strs[i].charAt(j)) {
+                    tempPrefix = tempPrefix + Character.toString(strs[i].charAt(j));
+                } else {
+                    break;
+                }
+
+            }
+            prefix = tempPrefix;
         }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"aaa", "aa", "aaa"};
+        System.out.println(longestCommonPrefixMehod(strs));
     }
 }
-*/
